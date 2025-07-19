bbe5c7e62e6d245a3f8d61caed0f6e272d01da29
goal651 <bugiriwilson651@gmail.com>
2025-07-19 19:14:20 +0200

    increase the size of discord bot

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -5,7 +5,7 @@ export default function LoadingScreen() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-[#313338] text-white">
             {/* Discord SVG Icon */}
-            <FaDiscord color="white"/>
+            <FaDiscord size={68} color="white"/>
             <h1 className="text-2xl font-bold mb-2">Loading Discord Bot...</h1>
             <div className="flex space-x-1 mt-2">
                 <span className="animate-bounce [animation-delay:-0.32s]">.</span>
